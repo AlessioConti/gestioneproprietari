db30d1217124ecd60fa17cdf7ba7651f72947cd0
AlessioConti <alessioconti02@yahoo.it>
2022-05-20 10:10:32 +0200

    Implementazione AutomobileServiceImpl

---
+++ src/it/prova/gestioneproprietari/service/automobile/AutomobileServiceImpl.java
@@ -0,0 +1,139 @@
+package it.prova.gestioneproprietari.service.automobile;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import it.prova.gestioneproprietari.dao.EntityManagerUtil;
+import it.prova.gestioneproprietari.dao.automobile.AutomobileDAO;
+import it.prova.gestioneproprietari.model.Automobile;
+
+public class AutomobileServiceImpl implements AutomobileService {
+
+	private AutomobileDAO automobileDAO;
+
+	public void setAutomobileDAO(AutomobileDAO automobileDAO) {
+		this.automobileDAO = automobileDAO;
+	}
+
+	public List<Automobile> listaTutteAutomobili() throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			automobileDAO.setEntityManager(entityManager);
+
+			return automobileDAO.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	public Automobile caricaSingolaAutomobile(Long id) throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			automobileDAO.setEntityManager(entityManager);
+
+			return automobileDAO.get(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	public void inserisci(Automobile automobileInstance) throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			entityManager.getTransaction().begin();
+
+			automobileDAO.setEntityManager(entityManager);
+
+			automobileDAO.insert(automobileInstance);
+
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	public void aggiorna(Automobile automobileInstance) throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			entityManager.getTransaction().begin();
+
+			automobileDAO.setEntityManager(entityManager);
+
+			automobileDAO.update(automobileInstance);
+
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	public void rimuovi(Long idAutomobileInstance) throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			entityManager.getTransaction().begin();
+
+			automobileDAO.setEntityManager(entityManager);
+
+			automobileDAO.delete(automobileDAO.get(idAutomobileInstance));
+
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	public List<Automobile> cercaTuttiConCFProprietarioIniziaCon(String iniziale) throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			automobileDAO.setEntityManager(entityManager);
+
+			return automobileDAO.findAllProprietarioConCFIniziaCon(iniziale);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	public List<Automobile> cercaTutteAutomobiliConProprietariMinorenni() throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			automobileDAO.setEntityManager(entityManager);
+
+			return automobileDAO.findAllAutomobiliConProprietariMinorenni();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+}
